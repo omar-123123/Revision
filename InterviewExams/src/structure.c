06de5fccff3446b6143c9ae4c12febe9cb84e946
omar-123123 <163739731+omar-123123@users.noreply.github.com>
2024-03-18 04:16:04 +0200

    Update structure.c

--- InterviewExams/src/structure.c
+++ InterviewExams/src/structure.c
@@ -1,9 +1,7 @@
 /******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+Auther: Abdulrhman Khalifa
+Working on: Eclipse IDE
+Description: Last Assignment of Structure Topic
 *******************************************************************************/
 
 #include <stdio.h>
